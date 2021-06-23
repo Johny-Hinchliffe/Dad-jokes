864ebe437e5f3502d0c6989680ab4c9c452299d4
Johny-Hinchliffe <82289369+Johny-Hinchliffe@users.noreply.github.com>
2021-06-23 17:19:58 +0100

    dad joke completed

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict'
+const jokeEl = document.querySelector('#joke')
+const jokeBtn = document.querySelector('#jokeBtn')
+
+// Using .then
+
+// const generateJoke = ()=> {
+//     const config = { 
+//         headers: {
+//             'Accept' : 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', config).then(res => res.json()).then(data => {
+//         jokeEl.innerHTML = data.joke
+//     })
+// }
+
+// Using Async/Await
+
+ async function generateJoke() {
+    const config = { 
+        headers: {
+            'Accept' : 'application/json'
+        }
+    }
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
+}
+
+
+jokeBtn.addEventListener('click', generateJoke)
